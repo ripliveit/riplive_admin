3dce2ab79002a9370c7bd6036c931d1fe7271b1e
gabrieledarrigo <darrigo.g@gmail.com>
2014-12-30 20:57:37 +0100

    Social users plugin use now namespaces

--- wp-content/plugins/rip-social-users/rip-social-users-plugin.php
+++ wp-content/plugins/rip-social-users/rip-social-users-plugin.php
@@ -9,41 +9,40 @@ namespace Rip_Social_Users;
   Version: 1.0
  */
 
-require_once ABSPATH . 'wp-content/plugins/rip-general/rip-general.php';
+require_once ABSPATH . 'wp-content/plugins/rip-general/rip-general-plugin.php';
 $autoloader = new \Rip_General\Classes\Rip_Autoloader(plugin_dir_path(__FILE__));
 
 /**
  * Social Users plugin.
  * 
  * @author Gabriele D'Arrigo - @acirdesign
- * @see rip_abstract_plugin
+ * @see \Rip_General\Classes\Rip_Abstract_Plugin
  */
 class Rip_Social_Users_Plugin extends \Rip_General\Classes\Rip_Abstract_Plugin {
-
+    
     /**
-     * Set all plugin configuration.
+     * Set plugin's configuration.
      */
     protected function _init() {
         $this->_ajax = array(
             'rip_social_users_get_social_user_by_email' => array(
-                'class' => 'rip_social_users_ajax_front_controller',
+                'class_name' => '\Rip_Social_Users\Controllers\Rip_Social_Users_Controller',
                 'method_name' => 'get_social_user_by_email',
             ),
             'rip_social_users_get_social_user_by_uuid' => array(
-                'class' => 'rip_social_users_ajax_front_controller',
+                'class_name' => '\Rip_Social_Users\Controllers\Rip_Social_Users_Controller',
                 'method_name' => 'get_social_user_by_uuid',
             ),
             'rip_social_users_insert_social_user' => array(
-                'class' => 'rip_social_users_ajax_front_controller',
+                'class_name' => '\Rip_Social_Users\Controllers\Rip_Social_Users_Controller',
                 'method_name' => 'insert_social_user',
             ),
         );
 
         register_activation_hook(__FILE__, array($this, 'activate'));
-
         register_deactivation_hook(__FILE__, array($this, 'deactivate'));
     }
 
 }
 
-$social_users = new \Rip_Social_Users\Rip_Social_Users_Plugin();
+$social_users_plugin = new \Rip_Social_Users\Rip_Social_Users_Plugin();

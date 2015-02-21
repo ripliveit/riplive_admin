ca45da5c603d965f49d40fe9a4edf4f8ee5dbde2
gabrieledarrigo <darrigo.g@gmail.com>
2015-02-21 10:24:47 +0100

    Added hihlight dependencies

--- wp-content/plugins/rip-general/classes/rip-di-container.php
+++ wp-content/plugins/rip-general/classes/rip-di-container.php
@@ -68,18 +68,18 @@ class Rip_Di_Container {
         $this->_container['transaction'] = function($c) {
             return new \Rip_General\Classes\Rip_Transaction();
         };
-                
+
         //
         // Author plugin's dependencies
         //
         $this->_container['authorsDao'] = function($c) {
             return new \Rip_Authors\Daos\Rip_Authors_Dao();
         };
-        
+
         $this->_container['authorsQueryService'] = function($c) {
             return new \Rip_Authors\Services\Rip_Authors_Query_Service($c['authorsDao']);
         };
-                
+
         //
         // Charts plugin's dependencies
         //
@@ -104,6 +104,20 @@ class Rip_Di_Container {
         $this->_container['chartsPersistsService'] = function($c) {
             return new \Rip_Charts\Services\Rip_Charts_Persist_Service(
                     $c['completeChartsDao'], $c['chartsQueryService'], $c['chartsVoteValidator'], $c['transaction']
+            );
+        };
+
+
+        //
+        // Songs plugin's dependencies
+        //
+        $this->_container['highlightsDao'] = function($c) {
+            return new \Rip_Highlights\Daos\Rip_Highlights_Dao();
+        };
+
+        $this->_container['highlightsQueryService'] = function($c) {
+            return new \Rip_Highlights\Services\Rip_Highlights_Query_Service(
+                    $c['highlightsDao'], $c['postsDao']
             );
         };
 

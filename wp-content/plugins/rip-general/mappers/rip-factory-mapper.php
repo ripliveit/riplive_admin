769086bd4d9def0ee14039315b0249b37d8b5a79
gabrieledarrigo <darrigo.g@gmail.com>
2015-02-24 20:40:01 +0100

    Use reflection to pass a variable number of arguments to factory

--- wp-content/plugins/rip-general/mappers/rip-factory-mapper.php
+++ wp-content/plugins/rip-general/mappers/rip-factory-mapper.php
@@ -18,7 +18,7 @@ class Rip_Factory_Mapper implements \Rip_General\Interfaces\Rip_Factory_Mapper_I
      * @var string 
      */
     protected static $_namespace = '\Rip_General\Mappers\\';
-    
+
     /**
      * Dinamically create a mapper.
      * 
@@ -26,14 +26,21 @@ class Rip_Factory_Mapper implements \Rip_General\Interfaces\Rip_Factory_Mapper_I
      * @return \class_name
      * @throws Exception
      */
-    public static function create_mapper($class_name, $arguments = null) {
+    public static function create_mapper($class_name) {
         if (empty($class_name)) {
             throw new Exception('Specify a mapper class name to create.');
         }
 
-        if (class_exists($class_name)) {
-            $mapper = new $class_name($arguments);
+        $args = func_get_args();
 
+        if (!empty($args)) {
+            $args = array_slice($args, 1);
+        }
+
+        if (class_exists($class_name)) {
+            $reflection = new \ReflectionClass($class_name);
+            $mapper     = $reflection->newInstanceArgs($args);
+            
             return $mapper;
         } else {
             throw new \Exception('Mapper ' . $class_name . ' was not found and cannot be created');
